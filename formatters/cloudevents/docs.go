ab3f7b166e036d155bc29f82b0f1777eac370137
Jim <jlambert@hashicorp.com>
2021-06-27 16:01:09 -0400

    add cloudevents package docs

---
+++ formatters/cloudevents/docs.go
@@ -0,0 +1,6 @@
+// cloudevents package includes a formatting Node which will transform and
+// encode Events into JSON or Text which conforms to the cloudevents spec.
+//
+// For more information about cloudevents see:
+// https://github.com/cloudevents/spec
+package cloudevents
