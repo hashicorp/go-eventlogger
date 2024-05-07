d8a1478bcbb20f4c48375258f250bdbd853c66fc
Uruemu Aganbi <uruemu@hashicorp.com>
2024-05-07 09:37:30 -0400

    add kafka testcontainer

---
+++ kafka_test.go
@@ -0,0 +1,43 @@
+package eventlogger
+
+import (
+	"context"
+	"fmt"
+	"log"
+
+	"github.com/testcontainers/testcontainers-go"
+	"github.com/testcontainers/testcontainers-go/modules/kafka"
+)
+
+func KafkaContainer() {
+	// runKafkaContainer {
+	ctx := context.Background()
+
+	kafkaContainer, err := kafka.RunContainer(ctx,
+		kafka.WithClusterID("test-cluster"),
+		testcontainers.WithImage("confluentinc/confluent-local:7.5.0"),
+	)
+	if err != nil {
+		log.Fatalf("failed to start container: %s", err)
+	}
+
+	// Clean up the container after
+	defer func() {
+		if err := kafkaContainer.Terminate(ctx); err != nil {
+			log.Fatalf("failed to terminate container: %s", err)
+		}
+	}()
+	// }
+
+	state, err := kafkaContainer.State(ctx)
+	if err != nil {
+		log.Fatalf("failed to get container state: %s", err) // nolint:gocritic
+	}
+
+	fmt.Println(kafkaContainer.ClusterID)
+	fmt.Println(state.Running)
+
+	// Output:
+	// test-cluster
+	// true
+}
