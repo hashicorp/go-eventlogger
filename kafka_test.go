c50f236b7d6c5546de8da556640a1b16a37f3fe2
Uruemu Aganbi <uruemu@hashicorp.com>
2024-05-07 16:33:16 -0400

    Revert "Implement kafka testcontainer with zookeeper"
    This reverts commit bdd316b709bd07b857055ea110e59c7b6d1e3a24.

--- kafka_test.go
+++ kafka_test.go
@@ -3,128 +3,116 @@ package eventlogger
 import (
 	"bytes"
 	"context"
-	"net/url"
 	"testing"
 	"time"
 
-	"context"
-	"os"
-
-	"github.com/stretchr/testify/require"
+	"github.com/IBM/sarama"
 	"github.com/testcontainers/testcontainers-go"
-	"github.com/testcontainers/testcontainers-go/network"
+	"github.com/testcontainers/testcontainers-go/modules/kafka"
 )
 
 func TestKafkaSink_Process(t *testing.T) {
 	ctx := context.Background()
 
-	kc := NewKafkaCluster(t, ctx)
-	kc.start(t, ctx)
-}
-
-const (
-	CLUSTER_NETWORK_NAME = "kafka-cluster"
-	ZOOKEEPER_PORT       = "2181"
-	KAFKA_BROKER_PORT    = "9092"
-	KAFKA_CLIENT_PORT    = "9093"
-	ZOOKEEPER_IMAGE      = "confluentinc/cp-zookeeper:5.2.1"
-	KAFKA_IMAGE          = "confluentinc/cp-kafka:5.2.1"
-)
-
-type KafkaCluster struct {
-	kafkaContainer     testcontainers.Container
-	zookeeperContainer testcontainers.Container
-}
-
-func NewKafkaCluster(t *testing.T, ctx context.Context) *KafkaCluster {
-	clusterNet, err := network.New(ctx, network.WithDriver("bridge"))
-	// creates a network, so kafka and zookeeper can communicate directly
-	require.NoError(t, err, "failed to create network")
-
-	zookeeperContainer := createZookeeperContainer(t, ctx, clusterNet)
-	kafkaContainer := createKafkaContainer(t, ctx, clusterNet)
-
-	return &KafkaCluster{
-		zookeeperContainer: zookeeperContainer,
-		kafkaContainer:     kafkaContainer,
+	kafkaContainer, err := kafka.RunContainer(ctx,
+		kafka.WithClusterID("test-cluster"),
+		testcontainers.WithImage("confluentinc/confluent-local:7.5.0"),
+	)
+	if err != nil {
+		t.Fatalf("failed to start kafka container: %v", err)
 	}
-}
 
-func (kc *KafkaCluster) start(t *testing.T, ctx context.Context) {
+	t.Cleanup(func() {
+		if err := kafkaContainer.Terminate(ctx); err != nil {
+			t.Fatalf("failed to terminate container: %v", err)
+		}
+	})
 
-	kc.zookeeperContainer.Start(ctx)
-	kc.kafkaContainer.Start(ctx)
-	kc.startKafka(t, ctx)
-}
+	brokers, err := kafkaContainer.Brokers(ctx)
+	if err != nil {
+		t.Fatalf("failed to query brokers: %v", err)
+	}
 
-func (kc *KafkaCluster) getKafkaHost(t *testing.T, ctx context.Context) string {
-	host, err := kc.kafkaContainer.Host(ctx)
-	require.NoError(t, err, "failed to get kafka host")
+	config := sarama.NewConfig()
+	config.Version = sarama.DefaultVersion
+	config.Producer.Return.Errors = true
+	config.Producer.Return.Successes = true
+	config.Consumer.Offsets.Initial = sarama.OffsetOldest
 
-	port, err := kc.kafkaContainer.MappedPort(ctx, KAFKA_CLIENT_PORT)
-	require.NoError(t, err, "failed to get kafka port")
+	client, err := sarama.NewClient(brokers, config)
+	if err != nil {
+		t.Fatalf("failed to create kafka client: %v", err)
+	}
+	t.Cleanup(func() {
+		if err = client.Close(); err != nil {
+			t.Fatalf("failed to close client: %v", err)
+		}
+	})
 
-	addr, err := url.JoinPath(host, port.Port())
-	require.NoError(t, err, "failed to join host and port")
-	return addr
-}
+	admin, err := sarama.NewClusterAdminFromClient(client)
+	if err != nil {
+		t.Fatalf("failed to create kafka admin client: %v", err)
+	}
 
-func (kc *KafkaCluster) startKafka(t *testing.T, ctx context.Context) {
-	kafkaStartFile, err := os.CreateTemp("", "testcontainers_start.sh")
-	require.NoError(t, err, "failed to create start file in temp directory")
-	defer os.Remove(kafkaStartFile.Name())
+	if err = admin.CreateTopic("test-topic", &sarama.TopicDetail{
+		NumPartitions:     1,
+		ReplicationFactor: 1,
+	}, false); err != nil {
+		t.Fatalf("failed to create topic: %v", err)
+	}
 
-	// needs to set KAFKA_ADVERTISED_LISTENERS with the exposed kafka port
-	exposedHost := kc.getKafkaHost(t, ctx)
-	kafkaStartFile.WriteString("#!/bin/bash \n")
-	kafkaStartFile.WriteString("export KAFKA_ADVERTISED_LISTENERS='PLAINTEXT://" + exposedHost + ",BROKER://kafka:" + KAFKA_BROKER_PORT + "'\n")
-	kafkaStartFile.WriteString(". /etc/confluent/docker/bash-config \n")
-	kafkaStartFile.WriteString("/etc/confluent/docker/configure \n")
-	kafkaStartFile.WriteString("/etc/confluent/docker/launch \n")
+	producer, err := sarama.NewSyncProducerFromClient(client)
+	if err != nil {
+		t.Fatalf("failed to create kafka producer: %v", err)
+	}
+	t.Cleanup(func() {
+		if err = producer.Close(); err != nil {
+			t.Fatalf("failed to close producer: %v", err)
+		}
+	})
 
-	err = kc.kafkaContainer.CopyFileToContainer(ctx, kafkaStartFile.Name(), "testcontainers_start.sh", 0700)
-	require.NoError(t, err, "failed to copy start file to container")
-}
+	sink := KafkaSink{
+		Format:   JSONFormat,
+		Topic:    "test-topic",
+		Producer: producer,
+	}
 
-func createZookeeperContainer(t *testing.T, ctx context.Context, network *testcontainers.DockerNetwork) testcontainers.Container {
-	// creates the zookeeper container, but do not start it yet
-	zookeeperContainer, err := testcontainers.GenericContainer(ctx, testcontainers.GenericContainerRequest{
-		ContainerRequest: testcontainers.ContainerRequest{
-			Image:          ZOOKEEPER_IMAGE,
-			ExposedPorts:   []string{ZOOKEEPER_PORT},
-			Env:            map[string]string{"ZOOKEEPER_CLIENT_PORT": ZOOKEEPER_PORT, "ZOOKEEPER_TICK_TIME": "2000"},
-			Networks:       []string{network.Name},
-			NetworkAliases: map[string][]string{network.Name: {"zookeeper"}},
+	evt := &Event{
+		Type:      "test",
+		CreatedAt: time.Now(),
+		Formatted: map[string][]byte{
+			JSONFormat: []byte(`"hello world"`),
+		},
+		Payload: []byte("hello world"),
+	}
+
+	_, err = sink.Process(ctx, evt)
+	if err != nil {
+		t.Fatalf("failed to process event: %v", err)
+	}
+
+	consumer, err := sarama.NewConsumerGroupFromClient("test-consumer", client)
+	if err != nil {
+		t.Fatalf("failed to create consumer: %v", err)
+	}
+	t.Cleanup(func() {
+		if err = consumer.Close(); err != nil {
+			t.Fatalf("failed to close consumer: %v", err)
+		}
+	})
+
+	err = consumer.Consume(ctx, []string{"test-topic"}, &testConsumer{
+		t: t,
+		assert: func(msg *sarama.ConsumerMessage) {
+			if msg.Topic != "test-topic" {
+				t.Fatalf("expected topic test-topic, got %s", msg.Topic)
+			}
+
+			if !bytes.Equal(evt.Formatted[JSONFormat], msg.Value) {
+				t.Fatalf("expected %s, got %s", evt.Formatted[JSONFormat], msg.Value)
+			}
 		},
 	})
-	require.NoError(t, err, "failed to create zookeeper container")
-
-	return zookeeperContainer
-}
-
-func createKafkaContainer(t *testing.T, ctx context.Context, network *testcontainers.DockerNetwork) testcontainers.Container {
-	// creates the kafka container, but do not start it yet
-	kafkaContainer, err := testcontainers.GenericContainer(ctx, testcontainers.GenericContainerRequest{
-		ContainerRequest: testcontainers.ContainerRequest{
-			Image:        KAFKA_IMAGE,
-			ExposedPorts: []string{KAFKA_CLIENT_PORT},
-			Env: map[string]string{
-				"KAFKA_BROKER_ID":                        "1",
-				"KAFKA_ZOOKEEPER_CONNECT":                "zookeeper:" + ZOOKEEPER_PORT,
-				"KAFKA_LISTENERS":                        "PLAINTEXT://0.0.0.0:" + KAFKA_CLIENT_PORT + ",BROKER://0.0.0.0:" + KAFKA_BROKER_PORT,
-				"KAFKA_LISTENER_SECURITY_PROTOCOL_MAP":   "BROKER:PLAINTEXT,PLAINTEXT:PLAINTEXT",
-				"KAFKA_INTER_BROKER_LISTENER_NAME":       "BROKER",
-				"KAFKA_OFFSETS_TOPIC_REPLICATION_FACTOR": "1",
-			},
-			Networks:       []string{network.Name},
-			NetworkAliases: map[string][]string{network.Name: {"kafka"}},
-			// the container only starts when it finds and run /testcontainers_start.sh
-			Cmd: []string{"sh", "-c", "while [ ! -f /testcontainers_start.sh ]; do sleep 0.1; done; /testcontainers_start.sh"},
-		},
-	})
-	require.NoError(t, err, "failed to create kafka container")
-
-	return kafkaContainer
 }
 
 type testConsumer struct {
