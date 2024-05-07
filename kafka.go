26f79391df0647c6a5b580b8457bc02c0e90b92d
David Bond <davidsbond93@gmail.com>
2024-05-07 20:56:15 +0100

    Add test for KafkaSink
    This commit adds a full end-to-end test of the KafkaSync which produces and consumes
    the message created by the sink. It modifies the struct to only accept a producer
    implementation and moves configuration to be the responsibility of the package
    consumer.
    
    Signed-off-by: David Bond <davidsbond93@gmail.com>

--- kafka.go
+++ kafka.go
@@ -9,20 +9,15 @@ import (
 )
 
 type KafkaSink struct {
-	// Brokers is a list of Kafka brokers to connect to
-	Broker []string
-
-	// Config contains configuration values for the Kafka producer.
-	Config *sarama.Config
-
 	// Format specifies the format the []byte representation is formatted in
 	// Defaults to JSONFormat
 	Format string
 
-	// Topic specifies the topic the message should be written to.
+	// Topic specifies the topic the event should be written to.
 	Topic string
 
-	producer sarama.SyncProducer
+	// The Producer is used to publish events onto a Kafka stream.
+	Producer sarama.SyncProducer
 }
 
 // Type describes the type of the node as a Sink.
@@ -30,15 +25,7 @@ func (_ *KafkaSink) Type() NodeType {
 	return NodeTypeSink
 }
 
-func (ks *KafkaSink) Process(ctx context.Context, e *Event) (*Event, error) {
-	if ks.producer == nil {
-		var err error
-		ks.producer, err = sarama.NewSyncProducer(ks.Broker, ks.Config)
-		if err != nil {
-			return nil, fmt.Errorf("failed to create kafka producer: %w", err)
-		}
-	}
-
+func (ks *KafkaSink) Process(_ context.Context, e *Event) (*Event, error) {
 	format := ks.Format
 	if format == "" {
 		format = JSONFormat
@@ -54,7 +41,7 @@ func (ks *KafkaSink) Process(ctx context.Context, e *Event) (*Event, error) {
 		Value: sarama.ByteEncoder(val),
 	}
 
-	if _, _, err := ks.producer.SendMessage(msg); err != nil {
+	if _, _, err := ks.Producer.SendMessage(msg); err != nil {
 		return nil, fmt.Errorf("failed to send message: %w", err)
 	}
 
--- kafka_test.go
+++ kafka_test.go
@@ -1,10 +1,12 @@
 package eventlogger
 
 import (
+	"bytes"
 	"context"
-	"fmt"
 	"testing"
+	"time"
 
+	"github.com/IBM/sarama"
 	"github.com/testcontainers/testcontainers-go"
 	"github.com/testcontainers/testcontainers-go/modules/kafka"
 )
@@ -26,15 +28,119 @@ func TestKafkaSink_Process(t *testing.T) {
 		}
 	})
 
-	state, err := kafkaContainer.State(ctx)
+	brokers, err := kafkaContainer.Brokers(ctx)
 	if err != nil {
-		t.Fatalf("failed to get kafka container state: %v", err)
+		t.Fatalf("failed to query brokers: %v", err)
 	}
 
-	fmt.Println(kafkaContainer.ClusterID)
-	fmt.Println(state.Running)
+	config := sarama.NewConfig()
+	config.Version = sarama.DefaultVersion
+	config.Producer.Return.Errors = true
+	config.Producer.Return.Successes = true
+	config.Consumer.Offsets.Initial = sarama.OffsetOldest
 
-	// Output:
-	// test-cluster
-	// true
+	client, err := sarama.NewClient(brokers, config)
+	if err != nil {
+		t.Fatalf("failed to create kafka client: %v", err)
+	}
+	t.Cleanup(func() {
+		if err = client.Close(); err != nil {
+			t.Fatalf("failed to close client: %v", err)
+		}
+	})
+
+	admin, err := sarama.NewClusterAdminFromClient(client)
+	if err != nil {
+		t.Fatalf("failed to create kafka admin client: %v", err)
+	}
+
+	if err = admin.CreateTopic("test-topic", &sarama.TopicDetail{
+		NumPartitions:     1,
+		ReplicationFactor: 1,
+	}, false); err != nil {
+		t.Fatalf("failed to create topic: %v", err)
+	}
+
+	producer, err := sarama.NewSyncProducerFromClient(client)
+	if err != nil {
+		t.Fatalf("failed to create kafka producer: %v", err)
+	}
+	t.Cleanup(func() {
+		if err = producer.Close(); err != nil {
+			t.Fatalf("failed to close producer: %v", err)
+		}
+	})
+
+	sink := KafkaSink{
+		Format:   JSONFormat,
+		Topic:    "test-topic",
+		Producer: producer,
+	}
+
+	evt := &Event{
+		Type:      "test",
+		CreatedAt: time.Now(),
+		Formatted: map[string][]byte{
+			JSONFormat: []byte(`"hello world"`),
+		},
+		Payload: []byte("hello world"),
+	}
+
+	_, err = sink.Process(ctx, evt)
+	if err != nil {
+		t.Fatalf("failed to process event: %v", err)
+	}
+
+	consumer, err := sarama.NewConsumerGroupFromClient("test-consumer", client)
+	if err != nil {
+		t.Fatalf("failed to create consumer: %v", err)
+	}
+	t.Cleanup(func() {
+		if err = consumer.Close(); err != nil {
+			t.Fatalf("failed to close consumer: %v", err)
+		}
+	})
+
+	err = consumer.Consume(ctx, []string{"test-topic"}, &testConsumer{
+		t: t,
+		assert: func(msg *sarama.ConsumerMessage) {
+			if msg.Topic != "test-topic" {
+				t.Fatalf("expected topic test-topic, got %s", msg.Topic)
+			}
+
+			if !bytes.Equal(evt.Formatted[JSONFormat], msg.Value) {
+				t.Fatalf("expected %s, got %s", evt.Formatted[JSONFormat], msg.Value)
+			}
+		},
+	})
+}
+
+type testConsumer struct {
+	t      *testing.T
+	assert func(msg *sarama.ConsumerMessage)
+}
+
+func (c *testConsumer) Setup(sarama.ConsumerGroupSession) error {
+	return nil
+}
+
+func (c *testConsumer) Cleanup(sarama.ConsumerGroupSession) error {
+	return nil
+}
+
+func (c *testConsumer) ConsumeClaim(session sarama.ConsumerGroupSession, claim sarama.ConsumerGroupClaim) error {
+	for {
+		select {
+		case message, ok := <-claim.Messages():
+			if !ok {
+				c.t.Fatalf("failed to recieve message")
+			}
+
+			c.assert(message)
+			session.MarkMessage(message, "")
+			return nil
+		case <-session.Context().Done():
+			return nil
+		}
+	}
 }
